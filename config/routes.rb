42c3908ee28518caf6892daac9a76f7b67be600f
Luis Bastidas <luis.basmar@gmail.com>
2012-11-16 11:48:18 -0500

    ffff

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Treebook::Application.routes.draw do
 
   resources :statuses
   get 'feed', to: 'statuses#index', as: :feed
-  root to: 'statuses#index'
+
+  root to: => 'statuses#index'
 
   get '/:id', to: 'profiles#show'
 
